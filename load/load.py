db7791c5a0dde46ae72deeade68ff84437838d74
Katie Repoli <168455578+krepoli@users.noreply.github.com>
2024-10-17 13:41:44 -0400

    add load/load.py

---
+++ load/load.py
@@ -0,0 +1,29 @@
+import os 
+import zipfile 
+import duckdb  
+
+
+def extract_zip_file(zip_file_path, target_dir): 
+    # implement logic to extract zip file here 
+    with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
+        zip_ref.extractall(target_dir)
+
+
+def load_json_gz_files_in_duck_db(json_dir): 
+    # implement logic to load json.gz files in duckdb here
+    con = duckdb.connect("github_stars.db")
+    con.sql("CREATE SCHEMA IF NOT EXISTS source")
+    con.sql(f"CREATE TABLE source.src_gharchive_new AS SELECT * FROM '{json_dir}/*.json.gz';")
+    #con.sql("CREATE TABLE source.src_gharchive_new AS SELECT * FROM 'gharchive_sample/*.json.gz';")
+    con.close()
+
+
+if __name__ == "__main__": 
+    data_dir = os.path.join(os.path.dirname(__file__), "..", "data") 
+
+    zip_file_path = os.path.join(data_dir, "gharchive_sample.zip") 
+    temp_folder = "./tmp" 
+    extract_zip_file(zip_file_path, temp_folder) 
+
+    unpacked_zip_dir = os.path.join(temp_folder, "gharchive_sample") 
+    load_json_gz_files_in_duck_db(unpacked_zip_dir)
