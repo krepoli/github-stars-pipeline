13dfe3ab2ffaf8072a9d48502842b4732b2e4fc4
Katie Repoli <168455578+krepoli@users.noreply.github.com>
2024-10-17 14:00:38 -0400

    add transform

---
+++ transform/models/marts/repo_pushes_daily.sql
@@ -0,0 +1,16 @@
+Select distinct 
+  repo_id,
+  repo_name,
+  date_day,
+  push_count,
+  sum(push_count) over (partition by repo_id order by date_day) AS cumul_push_count
+from (
+  Select
+    repo_id, 
+    repo_name, 
+    date_trunc('day', event_date) as date_day, 
+    count(*) as push_count 
+  from {{ ref('stg_gharchive') }} 
+  where event_type = 'Push'
+  group by repo_id, repo_name, date_day
+    )
---
+++ transform/models/staging/stg_gharchive.sql
@@ -0,0 +1,7 @@
+select
+  REPLACE(type, 'Event', '') as event_type, 
+  actor.login as user, 
+  repo.id as repo_id, 
+  repo.name as repo_name, 
+  created_at as event_date 
+from {{ source("gharchive", "src_gharchive_new") }}
