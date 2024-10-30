2eb869214d498b651abbcf52739e95fef8b615cc
Katie Repoli <168455578+krepoli@users.noreply.github.com>
2024-10-29 23:41:30 -0400

    Create macro

--- transform/models/marts/repo_pushes_daily.sql
+++ transform/models/marts/repo_pushes_daily.sql
@@ -1,9 +1,2 @@
-Select distinct 
-  repo_id,
-  repo_name,
-  date_trunc('day', event_date) as date_day,
-  count(*) as push_count,
-  sum(push_count) over (partition by repo_id order by date_day) AS cumul_push_count
-from {{ ref('stg_gharchive') }} 
-where event_type = 'Push'
-group by 1, 2, 3
+-- depends_on: {{ ref('stg_gharchive') }}
+{{ repo_event_by_period("push", "day") }}
--- transform/models/marts/repo_pushes_monthly.sql
+++ transform/models/marts/repo_pushes_monthly.sql
@@ -1,9 +1,2 @@
-Select distinct 
-  repo_id,
-  repo_name,
-  date_trunc('month', event_date) as date_month,
-  count(*) as push_count,
-  sum(push_count) over (partition by repo_id order by date_month) AS cumul_push_count
-from {{ ref('stg_gharchive') }} 
-where event_type = 'Push'
-group by 1, 2, 3
+-- depends_on: {{ ref('stg_gharchive') }}
+{{ repo_event_by_period("push", "month") }}
